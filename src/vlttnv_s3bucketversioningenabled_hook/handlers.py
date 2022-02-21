30f5d53e343a250dd9a5f3f57d97acafa01627da
Valentin Tunev <v.tunev@outlook.com>
2022-02-21 16:06:13 +0200

    Implement hook

--- src/vlttnv_s3bucketversioningenabled_hook/handlers.py
+++ src/vlttnv_s3bucketversioningenabled_hook/handlers.py
@@ -14,12 +14,45 @@ from cloudformation_cli_python_lib import (
 
 from .models import HookHandlerRequest, TypeConfigurationModel
 
-# Use this logger to forward log messages to CloudWatch Logs.
 LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
 TYPE_NAME = "Vlttnv::S3BucketVersioningEnabled::Hook"
 
 hook = Hook(TYPE_NAME, TypeConfigurationModel)
 test_entrypoint = hook.test_entrypoint
+
+def _isBucketExcluded(bucketName: str, excludedBucketSuffixes: str):
+    suffixesToExclude = [suffix.strip() for suffix in excludedBucketSuffixes.split(',')]
+    for suffix in suffixesToExclude:
+        LOG.info(f'**Checking if bucket name {bucketName} starts with {suffix}')
+        if suffix and bucketName.startswith(suffix):
+            return True
+    return False
+
+
+def _validate_object_versioning(s3Bucket: MutableMapping[str, Any], excludedBucketSuffixes: str) -> ProgressEvent:
+    status = OperationStatus.SUCCESS
+    message = ""
+    error_code = None
+
+    LOG.info(s3Bucket)
+    s3BucketName = s3Bucket.get("BucketName")
+
+    if s3BucketName and _isBucketExcluded(s3BucketName, excludedBucketSuffixes):
+        status = OperationStatus.SUCCESS
+        message = f"Object versioning is not required for bucket named: {s3BucketName}."
+
+    version_configuration = s3Bucket.get("VersioningConfiguration")
+    if version_configuration is None or version_configuration.get("Status") != "Enabled":
+        status = OperationStatus.FAILED
+        message = f"S3 bucket {s3BucketName} does not have object versioning enabled."
+
+    LOG.info(f"{status} - {message}")
+    return ProgressEvent(
+        status=status,
+        message=message,
+        errorCode=error_code
+    )
 
 
 @hook.handler(HookInvocationPoint.CREATE_PRE_PROVISION)
@@ -33,27 +66,22 @@ def pre_create_handler(
     progress: ProgressEvent = ProgressEvent(
         status=OperationStatus.IN_PROGRESS
     )
-    # TODO: put code here
+    target_name = request.hookContext.targetName
+    try:
+        if "AWS::S3::Bucket" == target_name:
+            progress = _validate_object_versioning(
+                request.hookContext.targetModel.get("resourceProperties"), 
+                type_configuration.excludedBucketSuffixes,
+            )
+        else:
+            raise exceptions.InvalidRequest(f"Unknown target type: {target_name}")
 
-    # Example:
-    # try:
-    #     # Reading the Resource Hook's target properties
-    #     resource_properties = target_model.get("resourceProperties")
+    except exceptions.InvalidRequest as e:
+        progress.status = OperationStatus.FAILED
+        progress.message = "Unknown target type: {target_name}"
+    except BaseException as e:
+        progress = ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"Unexpected error {e}")
 
-    #     if isinstance(session, SessionProxy):
-    #         client = session.client("s3")
-    #     # Setting Status to success will signal to cfn that the hook operation is complete
-    #     progress.status = OperationStatus.SUCCESS
-    # except TypeError as e:
-    #     # exceptions module lets CloudFormation know the type of failure that occurred
-    #     raise exceptions.InternalFailure(f"was not expecting type {e}")
-    #     # this can also be done by returning a failed progress event
-    #     # return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"was not expecting type {e}")
-
-    LOG.setLevel(logging.INFO)
-    LOG.info("(Create) Internal testing hook triggered for target: " + request.hookContext.targetName);
-
-    progress.status = OperationStatus.SUCCESS
     return progress
 
 
@@ -68,28 +96,23 @@ def pre_update_handler(
     progress: ProgressEvent = ProgressEvent(
         status=OperationStatus.IN_PROGRESS
     )
-    # TODO: put code here
 
-    # Example:
-    # try:
-    #     # A Hook that does not allow a resource's encryption algorithm to be modified
+    target_name = request.hookContext.targetName
+    try:
+        if "AWS::S3::Bucket" == target_name:
+            progress = _validate_object_versioning(
+                request.hookContext.targetModel.get("resourceProperties"), 
+                type_configuration.excludedBucketSuffixes,
+            )
+        else:
+            raise exceptions.InvalidRequest(f"Unknown target type: {target_name}")
 
-    #     # Reading the Resource Hook's target current properties and previous properties
-    #     resource_properties = target_model.get("resourceProperties")
-    #     previous_properties = target_model.get("previousResourceProperties")
+    except exceptions.InvalidRequest as e:
+        progress.status = OperationStatus.FAILED
+        progress.message = "Unknown target type: {target_name}"
+    except BaseException as e:
+        progress = ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"Unexpected error {e}")
 
-    #     if resource_properties.get("encryptionAlgorithm") != previous_properties.get("encryptionAlgorithm"):
-    #         progress.status = OperationStatus.FAILED
-    #         progress.message = "Encryption algorithm can not be changed"
-    #     else:
-    #         progress.status = OperationStatus.SUCCESS
-    # except TypeError as e:
-    #     progress = ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"was not expecting type {e}")
-
-    LOG.setLevel(logging.INFO)
-    LOG.info("(Update) Internal testing hook triggered for target: " + request.hookContext.targetName);
-
-    progress.status = OperationStatus.SUCCESS
     return progress
 
 
@@ -100,7 +123,6 @@ def pre_delete_handler(
         callback_context: MutableMapping[str, Any],
         type_configuration: TypeConfigurationModel
 ) -> ProgressEvent:
-    # TODO: put code here
     return ProgressEvent(
         status=OperationStatus.SUCCESS
     )
